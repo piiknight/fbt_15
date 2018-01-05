b7bd44095c960ea874258637b2c2b44624d79f2a
Pii Knight <tmtdien266@gmail.com>
2018-01-05 23:31:23 +0700

    finish login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,27 @@
+class SessionsController < ApplicationController
+  before_action :set_user, only: :create
+
+  def new; end
+
+  def create
+    if @user && @user.authenticate(params[:session][:password])
+      log_in @user
+      params[:session][:remember_me] == Settings.User.is_remember_pass ? remember(@user) : forget(@user)
+      redirect_to @user
+    else
+      flash[:danger] = t "error_creat"
+      render :new
+    end
+  end
+
+  def destroy
+    log_out if logged_in?
+    redirect_to root_url
+  end
+
+  private
+
+  def set_user
+    @user = User.find_by email: params[:session][:email].downcase
+  end
+end
